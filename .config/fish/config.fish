535ca7f3e62adf7133c2992bd8a3d4547d1f82cc
erik.juvan <erik.juvan@hotmail.com>
2023-11-14 23:34:23 +0100

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vim=nvim
+alias l='eza -a --group-directories-first'
+alias lw='l -T -L1'
+alias lww='lw -L2'
+alias lwww='lw -L3'
+alias lwwww='lw -L4'
+alias lwa='l -T'
