af5d09bc83311870757634e0f7aeabdfbd01fb17
erik.juvan <erik.juvan@gorenje.com>
2023-11-20 13:36:43 +0100

    Move from Packer to lazy.nvim

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,89 +1,58 @@
--- you must install packer first otherwise there will be errors
-return require("packer").startup(
-    function(use)
-        use("wbthomason/packer.nvim")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-        -- TJ created lodash of neovim
-        use("nvim-lua/plenary.nvim")
-        use("nvim-lua/popup.nvim")
-        use("nvim-telescope/telescope.nvim")
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
 
-        -- Nerdtree
-        use("preservim/nerdtree")
+require("lazy").setup({
+    'wbthomason/packer.nvim',
 
-        -- Treesitter
-        use("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
-        --use("nvim-treesitter/playground")
-        --use("romgrk/nvim-treesitter-context")
+    -- TJ created lodash of neovim
+    'nvim-lua/plenary.nvim',
+    'nvim-lua/popup.nvim',
+    'nvim-telescope/telescope.nvim',
 
-        -- Colorschemes
-        use("gruvbox-community/gruvbox")
-        use("folke/tokyonight.nvim")
-        --use({"catppuccin/nvim", as = "catppuccin"})
-        --use({"rose-pine/neovim", as = "rose-pine"})
-        use("tomasiser/vim-code-dark")
+    -- Nerdtree
+    'preservim/nerdtree',
 
-        -- cmp plugins
-        use("hrsh7th/nvim-cmp")
-        use("hrsh7th/cmp-buffer")
-        use("hrsh7th/cmp-path")
-        --use("hrsh7th/cmp-cmdline")
-        use("hrsh7th/cmp-nvim-lsp")
+    -- Treesitter
+    {
+        'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'
+    },
+    -- Colorschemes
+    'tomasiser/vim-code-dark',
+    --use("gruvbox-community/gruvbox")
+    --use("folke/tokyonight.nvim")
 
-        -- LSP
-        use("neovim/nvim-lspconfig")
-        use("williamboman/mason.nvim")
-        use("williamboman/mason-lspconfig.nvim")
+    -- cmp plugins
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    --use("hrsh7th/cmp-cmdline")
+    'hrsh7th/cmp-nvim-lsp',
 
-        use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-          require("toggleterm").setup()
-        end}
+    -- LSP
+    'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
 
-        -- Fugitive - git plugin
-        --use("tpope/vim-fugitive")
-    
-        -- Neovim statusline written in Lua
-        --use({
-        --    'nvim-lualine/lualine.nvim',
-        --    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-        --})
-    
-        -- trying copilot for 1 week
-        --use("github/copilot.vim")
-        --use("github/copilot.vim")
-        --use("zbirenbaum/copilot.lua");
-        --use({
-        --    "zbirenbaum/copilot-cmp",
-        --    after = {"copilot.lua"},
-        --    config = function ()
-        --        require("copilot_cmp").setup()
-        --    end
-        --});
-    
-        -- All the things
-        --use("neovim/nvim-lspconfig")
-        --use("hrsh7th/cmp-nvim-lsp")
-        --use("hrsh7th/cmp-buffer")
-        --use("hrsh7th/nvim-cmp")
-        --use{"tzachar/cmp-tabnine", run = "./install.sh", requires = 'hrsh7th/nvim-cmp'}
-        --use("onsails/lspkind-nvim")
-        --use("nvim-lua/lsp_extensions.nvim")
-        --use("glepnir/lspsaga.nvim")
-        --use("simrat39/symbols-outline.nvim")
-        --use("L3MON4D3/LuaSnip")
-        --use("saadparwaiz1/cmp_luasnip")
-
-        -- Primeagen doesn't create lodash
-        --use("ThePrimeagen/git-worktree.nvim")
-        --use("ThePrimeagen/harpoon")
-        --use("ThePrimeagen/refactoring.nvim")
-
-        --use("mbbill/undotree")
-
-        --use("mfussenegger/nvim-dap")
-        --use("rcarriga/nvim-dap-ui")
-        --use("theHamsta/nvim-dap-virtual-text")
-
-        --use("ranjithshegde/ccls.nvim")
-    end
-)
+    {
+        'akinsho/toggleterm.nvim', tag = '*',
+        config = function()
+            require("toggleterm").setup()
+        end
+    }
+})
